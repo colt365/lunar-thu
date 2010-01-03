4222029937d9e440306c3eb1796dbf63b945f75c
wuhaotsh <wuhaotsh@46a2c50e-c250-11de-b85e-e3865bbeca82>
2010-01-03 06:27:47 +0000

    Update of Issue29.

--- trunk/SmartMe/SmartMe/SmartMe.Windows/MiniWindow.xaml.cs
+++ trunk/SmartMe/SmartMe/SmartMe.Windows/MiniWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Threading;
+using SmartMe.Core.Data;
 
 namespace SmartMe.Windows
 {
@@ -128,12 +129,82 @@ namespace SmartMe.Windows
 
         private void MiniGrid_PreviewDragOver(object sender, System.Windows.DragEventArgs e)
         {
-        	MessageBox.Show("MiniGrid_PreviewDragOver! TODO: Please Add animation storyboard to shrink window -- TT!");
+        	//MessageBox.Show("MiniGrid_PreviewDragOver! TODO: Please Add animation storyboard to shrink window -- TT!");
         }
 
         private void MiniGrid_Drop(object sender, System.Windows.DragEventArgs e)
         {
-        	MessageBox.Show("MiniGrid_Drop! TODO: Please do query -- TT!");
+        	//MessageBox.Show("MiniGrid_Drop! TODO: Please do query -- TT!");
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Window_Drop:" + DateTime.Now.ToUniversalTime());
+            sb.AppendLine("sender:" + sender.ToString());
+            sb.AppendLine("e.Data.GetType:" + e.Data.GetType());
+            sb.AppendLine("e.Data.Formats:" + string.Join("; ", e.Data.GetFormats(true)));
+            sb.AppendLine("e.Source.GetType:" + e.Source.GetType());
+            sb.AppendLine("e.Source.ToString:" + e.Source.ToString());
+            sb.AppendLine("e.OriginalSource.GetType:" + e.OriginalSource.GetType());
+            sb.AppendLine("e.OriginalSource.ToString:" + e.OriginalSource.ToString());
+            sb.AppendLine("-- convert e.Data : --");
+            _mainWindow.ResultTextBox.Text = sb.ToString();
+            sb = new StringBuilder();
+
+            if (e.Data.GetDataPresent("Text", true))
+            {
+                sb.AppendLine("Text:" + e.Data.GetData("Text", true));
+                string text = e.Data.GetData("Text", true).ToString();
+                _mainWindow.InputTextBox.Text = text;
+                _mainWindow.DoDirectQuery(text, InputQueryType.Text);
+            }
+            if (e.Data.GetDataPresent("text/html", true))
+            {
+                sb.AppendLine("text/html:" + e.Data.GetData("text/html", true));
+            }
+            if (e.Data.GetDataPresent("text/x-moz-url", true))
+            {
+                sb.AppendLine("text/x-moz-url:" + e.Data.GetData("text/x-moz-url", true));
+            }
+            if (e.Data.GetDataPresent("text/html", true))
+            {
+                sb.AppendLine("text/html:" + e.Data.GetData("text/html", true));
+            }
+            if (e.Data.GetDataPresent("HTML Format", true))
+            {
+                sb.AppendLine("HTML Format:" + e.Data.GetData("HTML Format", true));
+            }
+            if (e.Data.GetDataPresent("UniformText", true))
+            {
+                sb.AppendLine("UniformText:" + e.Data.GetData("UniformText", true));
+            }
+            if (e.Data.GetDataPresent("FileName", true))
+            {
+                sb.AppendLine("FileName:" + e.Data.GetData("FileName", true));
+            }
+            if (e.Data.GetDataPresent("FileNameW", true))
+            {
+                sb.AppendLine("FileNameW:" + e.Data.GetData("FileNameW", true));
+            }
+            if (e.Data.GetDataPresent("FileName", true))
+            {
+                sb.AppendLine("FileName:" + string.Join("; ", (String[])e.Data.GetData("FileName", true)));
+            }
+            if (e.Data.GetDataPresent("FileNameW", true))
+            {
+                sb.AppendLine("FileNameW:" + string.Join("; ", (String[])e.Data.GetData("FileNameW", true)));
+            }
+            if (e.Data.GetDataPresent("UniformResourceLocator", true))
+            {
+                sb.AppendLine("UniformResourceLocator:" + e.Data.GetData("UniformResourceLocator", true));
+            }
+            if (e.Data.GetDataPresent("UniformResourceLocatorW", true))
+            {
+                sb.AppendLine("UniformResourceLocatorW:" + e.Data.GetData("UniformResourceLocatorW", true));
+            }
+
+            _mainWindow.ResultTextBox.Text += sb.ToString();
+            if (!_mainWindow.IsVisible)
+            {
+                ShowMainWindow();
+            }
         }
 		
         private void MiniGrid_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
